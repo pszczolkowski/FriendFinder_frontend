938db69e97240657e6b60f0fb17418083bc77ea8
Jan Godziatkowski <ph.godzio@gmail.com>
2015-11-21 17:43:25 +0100

    Create class to store constant values

---
+++ app/src/main/java/bart/friendfinderapp/shared/Constants.java
@@ -0,0 +1,9 @@
+package bart.friendfinderapp.shared;
+
+/**
+ * Created by Godzio on 2015-11-21.
+ */
+public class Constants {
+    public static final String APP_URL = "http://friend-finder-app.azurewebsites.net";
+    public static final int GLOBAL_TIMEOUT = 10000;
+}
