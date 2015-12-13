b3d08f98f099f980113f7d578db4eaaa4727df71
Jan Godziatkowski <ph.godzio@gmail.com>
2015-12-13 16:15:43 +0100

    Update app url (due to migration to new cloud account) and increase global timeout from 10s to 15s

--- app/src/main/java/bart/friendfinderapp/shared/Constants.java
+++ app/src/main/java/bart/friendfinderapp/shared/Constants.java
@@ -4,7 +4,7 @@ package bart.friendfinderapp.shared;
  * Created by Godzio on 2015-11-21.
  */
 public class Constants {
-    public static final String APP_URL = "http://friend-finder-app.azurewebsites.net";
-    public static final int GLOBAL_TIMEOUT = 10000;
+    public static final String APP_URL = "http://friend-finder-app2.azurewebsites.net";
+    public static final int GLOBAL_TIMEOUT = 15000;
     public static final String LOGIN_FILE = "LoginData";
 }
