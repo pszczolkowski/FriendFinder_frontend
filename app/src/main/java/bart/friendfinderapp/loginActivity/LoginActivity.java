1e002568af7cdfc7d59291bd84f4f2b0a44f8896
Jan Godziatkowski <ph.godzio@gmail.com>
2015-12-13 22:28:01 +0100

    Fix bug with opening wrong activity after mineralization

--- app/src/main/java/bart/friendfinderapp/loginActivity/LoginActivity.java
+++ app/src/main/java/bart/friendfinderapp/loginActivity/LoginActivity.java
@@ -93,6 +93,12 @@ public class LoginActivity extends Activity implements LoaderCallbacks< Cursor >
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
+        if ((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0) {
+            // Activity was brought to front and not created,
+            // Thus finishing this will get us to the last viewed activity
+            finish();
+            return;
+        }
         boolean fileReaded = readDataFromFile();
         if ( savedInstanceState != null && !fileReaded ) {
             if ( savedInstanceState.containsKey( "have_an_account" ) ) {
